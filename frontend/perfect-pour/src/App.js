b488a2d6b0697e4acc2e22ef6f86cf0fdce9936d
Jack Player <jackson.m.player@gmail.com>
2020-11-16 18:01:59 -0800

    Added Header component

--- frontend/perfect-pour/src/App.js
+++ frontend/perfect-pour/src/App.js
@@ -1,9 +1,11 @@
 import './styles/main.scss'
+import Header from './components/Reusable/Header'
 
 function App() {
+  const navList = ["home", "history", "calculations"]
   return (
     <div className="app">
-      
+      <Header navList={navList}/>
     </div>
   );
 }
--- frontend/perfect-pour/src/components/Header.js
+++ frontend/perfect-pour/src/components/Header.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Header = (navList) => {
+const Header = ({navList}) => {
+
+  const renderNavigation = (l) => {
+    const listItems = l.map((elem) => (
+      <li key={elem}>{elem}</li>
+    ))
+
+    return (
+      <ul>{listItems}</ul>
+    )
+  }
   return (
     <header id="header">
       <h1>The perfect pour</h1>
+      {navList && renderNavigation(navList)}
     </header>
   )
 }
 
 export default Header
+
+Header.propTypes = {
+  navList: PropTypes.arrayOf(PropTypes.string)
+}
