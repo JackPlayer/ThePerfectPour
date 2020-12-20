06f96669ea7b938c586d2bd5913272a3f3d6f4c3
Jack Player <jackson.m.player@gmail.com>
2020-12-20 12:30:23 -0800

    Added home page framework

--- frontend/perfect-pour/src/components/Home.js
+++ frontend/perfect-pour/src/components/Home.js
@@ -5,7 +5,29 @@ const Home = ({navList, active}) => {
   const username = "Jack"
   return (
     <Page active={active} pageTitle={`Welcome back ${username}...`} navList={navList}>
+      <div id="home-page">
+        <div id="progress-section">
+          <h2 className="sub-title">In progress</h2>
+            <div className="content-box">
+              
+            </div>
+        </div>
+        
+        <div id="create-section">
+          <div id="create-brew">
+            <h2 className="sub-title">New Brew</h2>
+            <div className="content-box">
+              
+            </div>
+          </div>
+          <div id="create-recipe">
+            <h2 className="sub-title">New Recipe</h2>
+            <div className="content-box">
 
+            </div>
+          </div>
+        </div>
+      </div>
     </Page>
   )
 }
