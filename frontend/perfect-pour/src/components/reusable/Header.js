c4f390349f1e4a261376d3ee69b62cdb6d681ff7
Jack Player <jackson.m.player@gmail.com>
2020-12-27 22:15:48 -0800

    Linted Header.js

--- frontend/perfect-pour/src/components/reusable/Header.js
+++ frontend/perfect-pour/src/components/reusable/Header.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+/**
+ * Header.js
+ * React component for the header used in the application
+ */
 
-const Header = ({navList, active}) => {
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Header = ({ navList, active }) => {
+  /**
+   * Markup for the navigation list
+   *
+   * @param l List of navigation items to render
+   */
 
   const renderNavigation = (l) => {
     const listItems = l.map((elem) => (
-      <li key={elem}><Link to={`/${elem}`} className={elem === active ? "active":""}>{elem}</Link></li>
-    ))
+      <li key={elem}><Link to={`/${elem}`} className={elem === active ? 'active' : ''}>{elem}</Link></li>
+    ));
 
     return (
       <ul id="main-navigation">{listItems}</ul>
-    )
-  }
+    );
+  };
   return (
     <header id="header">
       <h1>The perfect pour</h1>
       {navList && renderNavigation(navList)}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 Header.propTypes = {
-  navList: PropTypes.arrayOf(PropTypes.string)
-}
+  navList: PropTypes.arrayOf(PropTypes.string),
+  active: PropTypes.String,
+};
