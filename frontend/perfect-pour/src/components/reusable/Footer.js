9fae885326543fd89ef26a2418a53780707230a0
Jack Player <jackson.m.player@gmail.com>
2020-12-31 15:04:47 -0800

    Footer tests implemented

--- frontend/perfect-pour/src/components/reusable/Footer.js
+++ frontend/perfect-pour/src/components/reusable/Footer.js
@@ -6,7 +6,7 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer id="footer">
+    <footer data-testid="footer" id="footer">
       <p>Built with &hearts; in Victoria, BC</p>
       <p>&copy; 2020 The Perfect Pour</p>
     </footer>
---
+++ frontend/perfect-pour/src/tests/unit/Footer.test.js
@@ -0,0 +1,25 @@
+/**
+ * Footer.test.js
+ * Testing for the Footer component
+ */
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import Footer from '../../components/reusable/Footer';
+
+describe('<Footer />', () => {
+  it('has the site title', () => {
+    const component = render(<Footer />);
+    const footer = component.getByTestId('footer');
+    expect(footer).toHaveTextContent('The Perfect Pour');
+    expect(footer).toHaveTextContent('2020');
+  });
+
+  it('has the current year', () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const component = render(<Footer />);
+    const footer = component.getByTestId('footer');
+    expect(footer).toHaveTextContent(year);
+  });
+});
