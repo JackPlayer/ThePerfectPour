1d3ce61438175432a9888666c73f7c34c6df7e8a
= <jackson.m.player@gmail.com>
2020-12-30 20:32:30 -0800

    Added dateHandler functions

---
+++ frontend/perfect-pour/src/functions/dateHandler.js
@@ -0,0 +1,35 @@
+/**
+ * Checks if the input date is after the current date
+ *
+ * @param {Date} date Date to check
+ * @returns {boolean} true if the current date is after
+ */
+const isDateAfter = (date) => {
+  const currDate = new Date();
+  return currDate < date;
+};
+
+/**
+ * Checks if the input date is before the current date
+ *
+ * @param {Date} date Date to check
+ * @returns {boolean} true if the current date is before the input date
+ */
+const isDateBefore = (date) => {
+  const currDate = new Date();
+  return currDate > date;
+};
+
+/**
+ * Is the current date between dateStart and dateEnd?
+ *
+ * @param {Date} dateStart Start
+ * @param {Date} dateEnd End
+ * @returns {boolean} true if the current date is between start and end
+ */
+const isDateBetween = (dateStart, dateEnd) => {
+  const currDate = new Date();
+  return currDate >= dateStart && currDate <= dateEnd;
+};
+
+export default { isDateAfter, isDateBefore, isDateBetween };
