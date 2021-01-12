839a73740b43c3222bf2ede0b9595848371110ce
Jack <jackson.m.player@gmail.com>
2021-01-12 13:09:14 -0800

    create recipe query added

--- backend/perfect-pour-server/src/queries/recipeQueries.js
+++ backend/perfect-pour-server/src/queries/recipeQueries.js
@@ -16,9 +16,49 @@ const getRecipesFromUserID = (userID) => {
   };
 };
 
+const createRecipesFromUserID = (
+    userID, recipeID, recipeName, style,
+    type, sizeGal, yeast, description, hops,
+    additions, createdDate,
+) => {
+  const text = `INSERT INTO recipes (
+                        id, 
+                        user_id, 
+                        name, 
+                        style, 
+                        type, 
+                        size_gal, 
+                        yeast, 
+                        description, 
+                        hops,
+                        additions, 
+                        created_on
+                        ) VALUES (
+                          $1,
+                          $2,
+                          $3,
+                          $4,
+                          $5,
+                          $6,
+                          $7,
+                          $8,
+                          $9,
+                          $10,
+                          $11
+                        );`;
+  const values = [
+    recipeID, userID, recipeName, style, type,
+    sizeGal, yeast, description, hops, additions, createdDate,
+  ];
+  return {
+    text,
+    values,
+  };
+};
 
 module.exports = {
   recipeQueries: {
     getRecipesFromUserID,
+    createRecipesFromUserID,
   },
 };
